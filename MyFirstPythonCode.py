249c13c57c36eaba1e4c3bfbac0fa12f6a86682c
Ajay Vallabhi - a0v07r0 <Ajay.Vallabhi@walmart.com>
2024-01-28 18:32:28 -0600

    add new MyFirstPythonCode

---
+++ MyFirstPythonCode.py
@@ -0,0 +1,8 @@
+print("hello")
+#hello
+print("I love shanthi Priya")
+#I love shanthi Priya
+print('I love writing Python Programs')
+#I love writing Python Programs
+print('Its a great start on my first day with Python book with Rob Miles')
+#Its a great start on my first day with Python book with Rob Miles
